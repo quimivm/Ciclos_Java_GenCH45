0daf9b2b2d9b90170960f7184d9aee6949bb849b
quimivm <314001892@quimica.unam.mx>
2024-09-13 10:30:55 -0600

    se hace push del ejercicio inicial, se subirán más commits con la resolución completa

---
+++ src/ParDeNumeros.java
@@ -0,0 +1,13 @@
+public class ParDeNumeros {
+
+	public static void main(String[] args) {
+		for(int x=1; x<3; x++) {
+			System.out.println(x);
+				for (int y=1; y<3; y++) {
+					System.out.println(y);
+			}//for y
+		}//for x
+	}//main
+}//class
+
+
---
+++ src/Triángulo1.java
@@ -0,0 +1,12 @@
+//Triángulo hacia abajo
+public class Triángulo1 {
+	public static void main(String[] args) {
+
+		for(int x=1; x<3; x++) {
+			System.out.println(x);
+				for (int y=1; y<3; y++) {
+					System.out.println(y);
+			}//for y
+		}//for x
+	}//main
+}//class
